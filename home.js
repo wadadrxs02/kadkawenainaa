8fbce138676d8da57f210a422988e355149e9c2f
draxius2002 <nurawadah.ismail@gmail.com>
2022-11-13 05:10:13 +0800

    updated

--- home.js
+++ home.js
@@ -1,34 +1,3 @@
-var form = document.getElementById("sheetdb-form");
-form.addEventListener("submit", (e) => {
-  e.preventDefault();
-  fetch(form.action, {
-    method: "POST",
-    body: new FormData(document.getElementById("sheetdb-form")),
-  })
-    .then((response) => response.json())
-    .then((html) => {
-      // you can put any JS code here
-      alert("RSVP anda berjaya dihantar. Terima Kasih :) ");
-      form.reset();
-      $("#ModalRSVP").modal("hide");
-    });
-});
-
-$("#kedatangan").change(function () {
-  if ($(this).val() == "hadir") {
-    $("#jumlahkedatanganform").show();
-    $("#namaform").show();
-    $("#showcalendar").show();
-    $("#waktuform").show();
-  } else {
-    $("#namaform").hide();
-    $("#jumlahkedatanganform").hide();
-    $("#showcalendar").hide();
-    $("#waktuform").hide();
-    $("#ModalRSVP").modal("hide");
-  }
-});
-
 function updateTimer() {
   future = Date.parse("dec 11, 2022 11:00:00");
   now = new Date();
